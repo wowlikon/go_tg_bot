3e92c744c9053423100a08736cf8464435ff668b
wowlikon <wowlikon@gmail.com>
2024-03-26 23:41:58 +0300

    commands to functions

--- main.go
+++ main.go
@@ -12,7 +12,26 @@ import (
 	"github.com/joho/godotenv"
 )
 
+func GetIndex(s []string, e string) int {
+	for i := range s {
+		if s[i] == e {
+			return i
+		}
+	}
+	return -1
+}
+
 func main() {
+	//Проверка аргументов запуска
+	args := os.Args
+	fmt.Println(args)
+	if (GetIndex(args, "-h") != -1) || (GetIndex(args, "--help") != -1) {
+		fmt.Printf("Usage: %s [arguments]\n", args[0])
+		fmt.Println("\t-h --help  | help information")
+		fmt.Println("\t-d --debug | enable debug info")
+		return
+	}
+
 	//Загружаем .env
 	err := godotenv.Load(".env")
 	if err != nil {
@@ -35,7 +54,9 @@ func main() {
 	for update := range bot.GetUpdatesChan(u) {
 
 		//Вывод данных о сообщении
-		if update.Message == nil { continue }
+		if update.Message == nil {
+			continue
+		}
 		messageJSON, err := json.MarshalIndent(
 			update.Message, "", "  ",
 		)
@@ -43,7 +64,7 @@ func main() {
 			msg := tgbotapi.NewMessage(
 				update.Message.Chat.ID,
 				fmt.Sprintf(
-					"Error marshaling message to JSON:\n%s", err,
+					"Error marshaling message to JSON: \n%s", err,
 				),
 			)
 			bot.Send(msg)
@@ -61,18 +82,31 @@ func main() {
 				parts := strings.Split(update.Message.Text, " ")
 				switch parts[0] {
 				case "/start":
-					msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("Cmd: %s\n", parts))
-					bot.Send(msg)
-					break
+					start(bot, parts, update)
+				case "/help":
+					help(bot, update)
 				default:
-          msg := tgbotapi.NewMessage(update.Message.Chat.ID, "not cmd")
-					bot.Send(msg)
-					break
+					nocmd(bot, update)
 				}
 			} else {
-			  msg := tgbotapi.NewMessage(update.Message.Chat.ID, "not cmd")
-					bot.Send(msg)
+				msg := tgbotapi.NewMessage(update.Message.Chat.ID, "not cmd")
+				bot.Send(msg)
 			}
 		}
 	}
+}
+
+func start(bot *tgbotapi.BotAPI, parts []string, update tgbotapi.Update) {
+	msg := tgbotapi.NewMessage(update.Message.Chat.ID, fmt.Sprintf("Hello, %s", parts))
+	bot.Send(msg)
+}
+
+func help(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
+	msg := tgbotapi.NewMessage(update.Message.Chat.ID, "TODO")
+	bot.Send(msg)
+}
+
+func nocmd(bot *tgbotapi.BotAPI, update tgbotapi.Update) {
+	msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Error 404 command not found :(")
+	bot.Send(msg)
 }
